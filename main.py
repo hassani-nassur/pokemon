5e4bcec3b1143e83bcaad3558b8506bc1bf298ab
surnardo <hassaninasur44@gmail.com>
2023-04-02 20:09:58 +0200

    mis en place du visuel

--- main.py
+++ main.py
@@ -45,29 +45,32 @@ class Combat(Fichier):
         pockemone_image_player = tk.Label(self.fenetre,image=self.image_player)
         pockemone_image_player.place(x=50,y=300)
         
-        canvas_Element_player = tk.Canvas(self.fenetre,width=350,height=120,bg="#c2d4f6")
+        canvas_Element_player = tk.Canvas(self.fenetre,width=350,height=150,bg="#c2d4f6")
         canvas_Element_player.place(x=200,y=300)
         
         # affichage du vie 
         label_life_player = tk.Label(canvas_Element_player,text="Vie : {}".format(self.pockemone_player.get_point_life()),bg="#c2d4f6",font=('times new roman',14))
         label_life_player.place(x=10,y=10)
         
-        label_name_player = tk.Label(canvas_Element_player,text="Name : {}".format(self.pockemone_player.get_name()),bg="#c2d4f6",font=('times new roman',14))
-        label_name_player.place(x=10,y=30)
+        label_name_player = tk.Label(canvas_Element_player,text="Nom : {}".format(self.pockemone_player.get_name()),bg="#c2d4f6",font=('times new roman',14))
+        label_name_player.place(x=10,y=35)
         
-        label_puissance_attak_player = tk.Label(canvas_Element_player,bg="#c2d4f6",text="Puissance d'attaque : {}".format(self.pockemone_player.get_puissance_attaque()),font=('times new roman',14))
-        label_puissance_attak_player.place(x=10,y=50)
+        label_puissance_attak_player = tk.Label(canvas_Element_player,bg="#c2d4f6",text="Attaque : {}".format(self.pockemone_player.get_puissance_attaque()),font=('times new roman',14))
+        label_puissance_attak_player.place(x=10,y=60)
         
-        label_puissance_defence_player = tk.Label(canvas_Element_player,bg="#c2d4f6",text="Puissance de defence: {}".format(self.pockemone_player.get_defence()),font=('times new roman',14))
-        label_puissance_defence_player.place(x=10,y=70)
+        label_puissance_defence_player = tk.Label(canvas_Element_player,bg="#c2d4f6",text="Defence: {}".format(self.pockemone_player.get_defence()),font=('times new roman',14))
+        label_puissance_defence_player.place(x=10,y=85)
         
-        y = 100
-        x = 10
+        label_types_player = tk.Label(canvas_Element_player,bg="#c2d4f6",text="click un type D'attaque : ",font=('times new roman',14))
+        label_types_player.place(x = 10,y = 112)
+        
+        y = 112
+        x = 195
         for i in self.pockemone_player.get_type_names():
-            label_type_player = tk.Button(canvas_Element_player,text=i)
+            label_type_player = tk.Button(canvas_Element_player,text=i,font = ("Calibrie",12))
             label_type_player.place(x=x,y=y)
             
-            x +=50
+            x += 70
             if x == 300:
                 x = 10
                 y += 40
@@ -81,41 +84,45 @@ class Combat(Fichier):
         pockemone_image_ordinateur = tk.Label(self.fenetre,image=self.image_ordinateur)
         pockemone_image_ordinateur.place(x=450,y=50)
         
-        canvas_Element_ordinateur = tk.Canvas(self.fenetre,width=350,height=120,bg="#c2d4f6")
+        canvas_Element_ordinateur = tk.Canvas(self.fenetre,width=350,height=150,bg="#c2d4f6")
         canvas_Element_ordinateur.place(x=60,y=50)
         
         label_life_ordinateur = tk.Label(canvas_Element_ordinateur,bg="#c2d4f6",text="Vie : {}".format(self.pockemone_ordinateur.get_point_life()),font=('times new roman',14))
         label_life_ordinateur.place(x=10,y=10)
         
-        label_name_ordinateur = tk.Label(canvas_Element_ordinateur,text="Name : {}".format(self.pockemone_ordinateur.get_name()),bg="#c2d4f6",font=('times new roman',14))
-        label_name_ordinateur.place(x=10,y=30)
+        label_name_ordinateur = tk.Label(canvas_Element_ordinateur,text="Nom : {}".format(self.pockemone_ordinateur.get_name()),bg="#c2d4f6",font=('times new roman',14))
+        label_name_ordinateur.place(x=10,y=35)
         
         label_puissance_attak_ordinateur = tk.Label(canvas_Element_ordinateur,bg="#c2d4f6",text="Puissance d'attaque : {}".format(self.pockemone_ordinateur.get_puissance_attaque()),font=('times new roman',14))
-        label_puissance_attak_ordinateur.place(x=10,y=50)
+        label_puissance_attak_ordinateur.place(x=10,y=60)
         
         label_puissance_defence_ordinateur = tk.Label(canvas_Element_ordinateur,bg="#c2d4f6",text="Puissance de defence: {}".format(self.pockemone_ordinateur.get_defence()),font=('times new roman',14))
-        label_puissance_defence_ordinateur.place(x=10,y=70)
+        label_puissance_defence_ordinateur.place(x = 10,y = 85)
         
-        y = 100
-        x = 10
+        label_types_ordinateur = tk.Label(canvas_Element_ordinateur,bg="#c2d4f6",text="Types :",font=('times new roman',14))
+        label_types_ordinateur.place(x = 10,y = 112)
+        
+        y = 112
+        x = 80
         
         for i in self.pockemone_ordinateur.get_type_names():
-            label_type_ordinateur = tk.Label(canvas_Element_ordinateur,text=i,bg="#c496f0")
+            label_type_ordinateur = tk.Label(canvas_Element_ordinateur,text=i,bg="#c496f0",font = ("Calibrie",12))
             label_type_ordinateur.place(x=x,y=y)
             
-            x +=50
+            x += 60
             if x == 300:
                 x = 10
                 y += 40
-        
+       
+        # affichage du gagnant et du perdant
         if(not self.__est_vivant(self.pockemone_ordinateur) or not self.__est_vivant(self.pockemone_player)):
-            label_win = tk.Label(self.fenetre,text="le gagnant est : {}".format(self.get_name_winner()),font=("times new roman",15))
-            label_win.place(x=30,y=200)
+            label_win = tk.Label(self.fenetre,text=" {} à Gangner".format(self.get_name_winner()),font=("times new roman",15),bg="#9cf6c0",width=25)
+            label_win.place(x=150,y=230)
             
-            label_win = tk.Label(self.fenetre,text="le perdant est : {}".format(self.get_name_loser()),font=("times new roman",15))
-            label_win.place(x=30,y=220)
+            label_win = tk.Label(self.fenetre,text=" {} à perdu".format(self.get_name_loser()),font=("times new roman",15),bg="#f8aeae",width=25)
+            label_win.place(x=150,y=260)
             
-        
+    # menue pour revenir à l'acceuil 
     def menu_game(self):
         menu = tk.Menu(self.fenetre)
         menu.add_command(label="Accueil",command=self.run)
@@ -129,22 +136,24 @@ class Combat(Fichier):
         self.clean_fenetre()
         
         self.get_pockemon()
-        canvas = tk.Canvas(self.fenetre,width=300,height=300)
-        canvas.pack(padx=12,pady=32)
+        canvas = tk.Canvas(self.fenetre,bg="#e2e2fc")
+        canvas.pack(padx=12,pady=100)
         
-        label = tk.Label(canvas,text="Choisez un pockemone").pack()
+        label = tk.Label(canvas,text="Choisie Ton pokemone",font=("temes new roman",14),bg="#e2e2fc")
+        label.place(x=80,y=50)
         
         global list_pockemone
-        list_pockemone = ttk.Combobox(canvas,state='readonly')
+        list_pockemone = ttk.Combobox(canvas,state='readonly',font=("times new roman",15))
         list_pockemone["values"] = tuple(self.names_pockemone)
         list_pockemone.current(0)
-        list_pockemone.pack()
+        list_pockemone.place(x=80,y=90)
         
-        button = tk.Button(self.fenetre,text="Valider",command=self.create_pockemone)
-        button.pack()
+        button = tk.Button(self.fenetre,text="Valider",command=self.create_pockemone,font=("times new roman",12))
+        button.place(x=280,y=250)
         
         self.fenetre.mainloop()
-
+   
+    # methode declancher lors d'un evenement
     def batail(self,event):
         
         if(type( event.widget) is tk.Button ):
@@ -159,18 +168,20 @@ class Combat(Fichier):
             self.duel(self.pockemone_player,self.pockemone_ordinateur)
             self.duel(self.pockemone_ordinateur,self.pockemone_player)
     
-    
+    # methode servant à verifier si un pockemone est vivant
     def __est_vivant(self,pockemone):
         if(pockemone.get_point_life() <= 0):
             return False
         else:
             return True
-    
+   
+    # methode servant de balais de fenetre 
     def clean_fenetre(self):
         for widget in self.fenetre.winfo_children():
             if str(widget) !=".!menu":
                 widget.destroy()
-    
+   
+    # methode servant à diminuer les points de vie
     def duel(self, pockemone_one, pockemone_two):
         
         vie_pock_two = pockemone_two.get_point_life()
@@ -179,6 +190,7 @@ class Combat(Fichier):
         type_defence = pockemone_two.get_type_defence()
         
         point_attaque = pockemone_one.get_puissance_attaque()
+        point_defence = pockemone_two.get_defence()
         
         info_type = self.get_data_files("db/types.json")
         
@@ -194,11 +206,14 @@ class Combat(Fichier):
                     else:
                         vie_pock_two = 0
                         pockemone_two.set_point_life(vie_pock_two)
-            
+            # SI Le type d'attaque et celle de defence ne sont pas dans le fichier type.json on utilise une autre methode pour calculer les points d'attaque
             if(not type_exist):
-            
-                vie_pock_two -= point_attaque
-                if(vie_pock_two >=0):
+                point_diminuer = point_attaque - point_defence
+                if point_diminuer <= 0:
+                    vie_pock_two -= 10
+                else:
+                    vie_pock_two -= point_diminuer
+                if(vie_pock_two >= 0):
                     pockemone_two.set_point_life(vie_pock_two)
                 else:
                     vie_pock_two = 0
@@ -206,7 +221,8 @@ class Combat(Fichier):
                 
             self.clean_fenetre()
             self.display()
-        
+    
+    # methode qui renvoi le nom du gagnant 
     def get_name_winner(self):
         if not self.__est_vivant(self.pockemone_ordinateur):
             return self.pockemone_player.get_name()
@@ -214,7 +230,7 @@ class Combat(Fichier):
             return self.pockemone_ordinateur.get_name()
         else:
             return False
-        
+    # methode qui renvoi le nom du perdant
     def get_name_loser(self):
         
         if not self.__est_vivant(self.pockemone_ordinateur):
